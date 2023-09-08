776a0b5a3736a7547cd327e17ae626ec841f97d9
GhwFluffy <ghw@ghwfluffy.com>
2023-09-07 22:17:57 -0500

    Publish script don't publish itself

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 set -eux -o pipefail
 
-ssh fluffyco.in "mkdir -p /var/www/html/test"
-scp -r ./* fluffyco.in:/var/www/html/test/
+ssh fluffyco.in "rm -rf /var/www/html/test; mkdir -p /var/www/html/test"
+scp -r $(ls ./* | grep -v "$(basename "${0}")") fluffyco.in:/var/www/html/test/
