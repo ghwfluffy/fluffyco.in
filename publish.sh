2145cd68683d9651836ec9195d3679c28176b9c0
GhwFluffy <ghw@ghwfluffy.com>
2023-09-07 23:03:59 -0500

    Updating publish script.

--- publish.sh
+++ publish.sh
@@ -4,5 +4,6 @@ set -eux -o pipefail
 
 cp -v ../fluffycoin/README.md ./readme/
 
-ssh fluffyco.in "rm -rf /var/www/html/test; mkdir -p /var/www/html/test"
-GLOBIGNORE=".git:publish.sh:*.swp" scp -r ./* fluffyco.in:/var/www/html/test/
+ssh fluffyco.in "rm -rf /var/www/html/*"
+scp -r ./* fluffyco.in:/var/www/html/
+ssh fluffyco.in "rm -f /var/www/html/publish.sh"
